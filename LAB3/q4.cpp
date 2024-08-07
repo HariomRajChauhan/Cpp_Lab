a801dfca265b631506edeac8b1e6ceaf4f143c70
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2024-08-08 02:58:46 +0545

    q4 and q5 problem solved

--- LAB3/q4.cpp
+++ LAB3/q4.cpp
@@ -1,38 +1,97 @@
 // 4. Program to convert time (hour, minute and second) in 24-hr format to 12-hr format.
 
+// class to class data type conversion
+// source class = Time24
+// destination class = Time12
+
+// source class --> destination class
+
 
 #include <iostream>
+
 using namespace std;
 
-class Time24 {
+class Time24
+{
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
 public:
-    int hour, minute, second;
+    Time24() : hours(0), minutes(0), seconds(0) {}
+    Time24(int h, int m, int s) : hours(h), minutes(m), seconds(s) {}
 
-    Time24(int hour, int minute, int second) : hour(hour), minute(minute), second(second) {}
+    void Display()
+    {
+        cout << "Time24: " << hours << ":" << minutes << ":" << seconds << endl;
+    }
 
-    void to12HourFormat() {
-        string period = "AM";
-        int hour12 = hour;
-        if (hour >= 12) {
-            period = "PM";
-            if (hour > 12) hour12 -= 12;
-        }
-        if (hour == 0) hour12 = 12;
-        cout << "24-hour format: " << hour << ":" << minute << ":" << second << " = 12-hour format: " << hour12 << ":" << minute << ":" << second << " " << period << endl;
+    int getHours() const
+    {
+        return hours;
+    }
+    int getMinutes() const
+    {
+        return minutes;
+    }
+    int getSeconds() const
+    {
+        return seconds;
     }
 };
 
-int main() {
-    int hour, minute, second;
-    cout << "Enter hour: ";
-    cin >> hour;
-    cout << "Enter minute: ";
-    cin >> minute;
-    cout << "Enter second: ";
-    cin >> second;
+class Time12
+{
+private:
+    int hours;
+    int minutes;
+    int seconds;
+    string meridiem;
 
+public:
+    Time12() : hours(0), minutes(0), seconds(0), meridiem("AM") {}
+    Time12(int h, int m, int s) : hours(h), minutes(m), seconds(s)
+    {
+        if (hours >= 12)
+        {
+            meridiem = "PM";
+            hours -= 12;
+        }
+        else
+        {
+            meridiem = "AM";
+        }
+    }
 
-    Time24 time(hour, minute, second);
-    time.to12HourFormat();
+    void Display()
+    {
+        cout << "Time12: " << hours << ":" << minutes << ":" << seconds << " " << meridiem << endl;
+    }
+
+    Time12(const Time24 &t)
+    {
+        hours = t.getHours();
+        minutes = t.getMinutes();
+        seconds = t.getSeconds();
+        if (hours >= 12)
+        {
+            meridiem = "PM";
+            hours -= 12;
+        }
+        else
+        {
+            meridiem = "AM";
+        }
+    }
+};
+
+int main()
+{
+    Time24 t(13, 30, 45);
+    Time12 t2 = t;
+    t.Display();
+    t2.Display();
+
     return 0;
 }
--- LAB3/q5.cpp
+++ LAB3/q5.cpp
@@ -1,37 +1,88 @@
 // 5. Program to convert time (hour, minute and second) in 12-hr format to 24-hr format.
 
+// class to class data type conversion
+// source class = Time12
+// destination class = Time24
+
+// source class --> destination class
+
 #include <iostream>
+
 using namespace std;
 
-class Time12 {
+class Time12
+{
+private:
+    int hours;
+    int minutes;
+    int seconds;
+    string meridiem;
+
 public:
-    int hour, minute, second;
-    string period;
+    Time12() : hours(0), minutes(0), seconds(0), meridiem("AM") {}
+    Time12(int h, int m, int s, string mer) : hours(h), minutes(m), seconds(s), meridiem(mer) {}
 
-    Time12(int hour, int minute, int second, string period) : hour(hour), minute(minute), second(second), period(period) {}
+    void Display()
+    {
+        cout << "Time12: " << hours << ":" << minutes << ":" << seconds << " " << meridiem << endl;
+    }
 
-    void to24HourFormat() {
-        int hour24 = hour;
-        if (period == "PM") {
-            if (hour != 12) hour24 += 12;
-        } else if (hour == 12) hour24 = 0;
-        cout << "12-hour format: " << hour << ":" << minute << ":" << second << " " << period << " = 24-hour format: " << hour24 << ":" << minute << ":" << second << endl;
+    int getHours() const
+    {
+        return hours;
+    }
+    int getMinutes() const
+    {
+        return minutes;
+    }
+    int getSeconds() const
+    {
+        return seconds;
+    }
+    string getMeridiem() const
+    {
+        return meridiem;
     }
 };
 
-int main() {
-    int hour, minute, second;
-    string period;
-    cout << "Enter hour: ";
-    cin >> hour;
-    cout << "Enter minute: ";
-    cin >> minute;
-    cout << "Enter second: ";
-    cin >> second;
-    cout << "Enter period (AM/PM): ";
-    cin >> period;
 
-    Time12 time(hour, minute, second, period);
-    time.to24HourFormat();
+class Time24
+{
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+public:
+    Time24() : hours(0), minutes(0), seconds(0) {}
+    Time24(int h, int m, int s) : hours(h), minutes(m), seconds(s) {}
+
+    Time24(const Time12 &t12)
+    {
+        hours = t12.getHours();
+        minutes = t12.getMinutes();
+        seconds = t12.getSeconds();
+        if (t12.getMeridiem() == "PM")
+        {
+            hours += 12;
+        }
+    }
+
+    void Display()
+    {
+        cout << "Time24: " << hours << ":" << minutes << ":" << seconds << endl;
+    }
+};
+
+
+
+int main()
+{
+    Time12 t12(5, 30, 45, "PM");
+    t12.Display();
+
+    Time24 t24 = t12;
+    t24.Display();
+
     return 0;
 }

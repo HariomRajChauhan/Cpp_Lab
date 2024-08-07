208dcc2d07bc05d19b1639fc42a4fd83609f9722
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2024-08-08 02:45:15 +0545

    q 3 ko lab3 ko bhayo

--- LAB3/q3.cpp
+++ LAB3/q3.cpp
@@ -1,30 +1,64 @@
 // 3. Program to convert Cartesian coordinate to Polar.
 
+// class to class data type conversion
+// source class = CartesianCoordinate
+// destination class = PolarCoordinate
+
+// source class --> destination class
+
 
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
-class CartesianCoordinate {
-public:
-    double x, y;
+class CartesianCoordinate
+{
+private:
+    double x;
+    double y;
 
+public:
+    CartesianCoordinate() : x(0), y(0) {}
     CartesianCoordinate(double x, double y) : x(x), y(y) {}
 
-    void toPolar() {
-        double r = sqrt(pow(x, 2) + pow(y, 2));
-        double theta = atan(y / x) * 180.0 / M_PI;
-        cout << "Cartesian coordinates (x=" << x << ", y=" << y << ") = Polar coordinates (r=" << r << ", theta=" << theta << ")" << endl;
+    double getX() const
+    {
+        return x;
+    }
+    double getY() const
+    {
+        return y;
     }
 };
 
-int main() {
-    double x, y;
-    cout << "Enter x: ";
-    cin >> x;
-    cout << "Enter y: ";
-    cin >> y;
-    CartesianCoordinate cartesian(x, y);
-    cartesian.toPolar();
+class PolarCoordinate
+{
+private:
+    double radius;
+    double angle;
+
+public:
+    PolarCoordinate() : radius(0), angle(0) {}
+    PolarCoordinate(double r, double a) : radius(r), angle(a) {}
+
+    void Display()
+    {
+        cout << "Polar Coordinate: (" << radius << "," << angle << ")" << endl;
+    }
+
+    PolarCoordinate(const CartesianCoordinate &c)
+    {
+        radius = sqrt(pow(c.getX(), 2) + pow(c.getY(), 2));
+        angle = atan(c.getY() / c.getX()) * 180 / 3.14159;
+    }
+};
+
+int main()
+{
+    CartesianCoordinate c(3, 3);
+    PolarCoordinate p = c;
+    p.Display();
+
     return 0;
 }
